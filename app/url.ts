ed53540c7da25807963aba1dd0b01bfea8e7519e
PETER IRUNGU <peteralidante254@gmail.com>
2024-08-12 18:45:50 +0300

    Refactor: Update API_URL export in url.ts

--- app/url.ts
+++ app/url.ts
@@ -1,4 +1,2 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_URL
-    : process.env.Next_LOCAL_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
+
